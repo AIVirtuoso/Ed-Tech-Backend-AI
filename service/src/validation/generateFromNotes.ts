124cf3f2368a4cc77275d241162e3de60449160e
goldvlady <vladyslavabramov@outlook.com>
2023-09-07 14:44:20 +0100

    shepherd ai

--- service/src/validation/generateFromNotes.ts
+++ service/src/validation/generateFromNotes.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 const docsSchema = z.object({
   body: z.object({
     studentId: z.string({ required_error: 'studentId is required' }),
-    documentId: z.string({ required_error: 'documentId is required' }),
+    note: z.string({ required_error: 'note is required' }),
     count: z.number({
       required_error: 'number of flash cards to generate must be specified'
     }),
