10208eec50b0b96721df912782721035c6a5d635
goldvlady <vladyslavabramov@outlook.com>
2023-09-20 17:46:33 +0100

    fix prompt

--- service/src/helpers/llmFunctions/getConversationDescription.ts
+++ service/src/helpers/llmFunctions/getConversationDescription.ts
@@ -5,10 +5,18 @@ import config from 'config';
 import { ConversationChain } from 'langchain/chains';
 
 const promptTemplate = `
-Can you structure a very concise but precise takeaway on our discussion using these headers 
+Given the context and conversation below, Can you generate a very concise but single statement stating your problems with what you haven't yet. the statement should contain these two information.
+1. What you needs help with
+2. What you understands
 
-1. What student needs help with
-2. What student understands
+Context: In this conversation, a student stating what your problems learning an issue.
+
+some rules.
+- Don't state your identity.
+- Don't break character.
+- Speak in first person only. 
+- Speak like you need help
+
 here is the history of the conversation: {history}
 here is the input of all chats each conversation seperated by a dash(-): {input}
 `;
