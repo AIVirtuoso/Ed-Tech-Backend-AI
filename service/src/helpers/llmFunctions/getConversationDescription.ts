ad56ce2da83fb9f5ef87daaf12d9c4125c84487a
goldvlady <vladyslavabramov@outlook.com>
2023-09-21 17:40:53 +0100

    prompt

--- service/src/helpers/llmFunctions/getConversationDescription.ts
+++ service/src/helpers/llmFunctions/getConversationDescription.ts
@@ -5,17 +5,16 @@ import config from 'config';
 import { ConversationChain } from 'langchain/chains';
 
 const promptTemplate = `
-Given the context and conversation below, Can you generate a very concise but single statement stating your problems with what you haven't yet. the statement should contain these two information.
-1. What you needs help with
-2. What you understands
-
-Context: In this conversation, a student stating what your problems learning an issue.
-
-some rules.
+Given the context and conversation above, can you generate a very concise but precise set of statements using these headers .
+1. What I need help with
+2. What I understand
+Context: In this conversation, a student is stating what their problems learning a topic.
+Some rules:
 - Don't state your identity.
 - Don't break character.
-- Speak in first person only. 
+- Speak in first person only.
 - Speak like you need help
+- Go straight to the two statements. Don't provide a preamble
 
 here is the history of the conversation: {history}
 here is the input of all chats each conversation seperated by a dash(-): {input}
