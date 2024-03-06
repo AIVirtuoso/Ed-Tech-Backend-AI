d79777025e370a31a06609a09d2101023c269ed8
goldvlady <vladyslavabramov@outlook.com>
2024-03-06 11:40:57 +0100

    uppercase shep api key

--- ai/app/dependencies/auth.py
+++ ai/app/dependencies/auth.py
@@ -3,7 +3,7 @@ from typing_extensions import Annotated
 import os
 
 async def check_shepherd_header(x_shepherd: Annotated[str, Header()]):
-    if x_shepherd is None or x_shepherd != os.getenv("shep_api_key"):
+    if x_shepherd is None or x_shepherd != os.getenv("SHEP_API_KEY"):
         raise HTTPException(
             status_code=401,
             detail="Invalid X-Shepherd-Header provided",
