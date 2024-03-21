d7ce5eb0c99f8a8929d5e20f12c707ba29ab903e
goldvlady <vladyslavabramov@outlook.com>
2024-03-21 23:47:17 +0100

    move DB stuff to background task

--- ai/app/routers/maths.py
+++ ai/app/routers/maths.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Request
+from fastapi import APIRouter, BackgroundTasks
 from fastapi.responses import JSONResponse, StreamingResponse
 from pydantic import BaseModel
 from sqlmodel import Session, select
@@ -83,15 +83,56 @@ def stream_openai_chunks(chunks: str, body: StudentConversation):
     save_initial_message(initial_message, body)
     create_conversation_title(initial_message, body)
     yield "done with stream"
-
-def chunk_text(text: str, chunk_size=50):
-    """Generator function to chunk text into smaller parts."""
-    for i in range(0, len(text), chunk_size):
-        yield text[i:i + chunk_size]
+def write_to_db(body,user_msg, steps, tc, assistant_resp, assistant_resp_for_tc):
+  print("background job 1 fires")
+  with Session(engine) as session:
+        user_message = ConversationLogs(studentId=body.studentId, conversationId=UUID(body.conversationId), log=user_msg)  
+        session.add(user_message)
+        session.commit()
+        if tc.get("role") == "function":
+          # save tc 
+          print("tool call",tc)
+          user_message = ConversationLogs(studentId=body.studentId, conversationId=UUID(body.conversationId), log=tc)  
+          session.add(user_message)
+          session.commit()
+        if len(assistant_resp) != 0 and assistant_resp is not None: 
+          assistant_msg = wrap_for_ql('assistant', assistant_resp)
+          bot_message = ConversationLogs(studentId=body.studentId, conversationId=UUID(body.conversationId), log=assistant_msg)
+          session.add(bot_message)
+          session.commit()
+        
+        if len(assistant_resp_for_tc) != 0 and assistant_resp_for_tc is not None: 
+          print("basically outside steps")
+          print(assistant_resp_for_tc)
+          history = build_chat_history(assistant_resp_for_tc, body.query)
+          is_solved = steps_agent(history, steps)
+          assistant_msg = wrap_for_ql('assistant', assistant_resp_for_tc, is_solved)
+          bot_message = ConversationLogs(studentId=body.studentId, conversationId=UUID(body.conversationId), log=assistant_msg)
+          session.add(bot_message)
+          session.commit()
+          print(assistant_msg)
+  
+def write_to_db_with_steps(body,user_msg, updated_messages, steps, assistant_resp_for_tool_call):
+    print("background job 2 fires")
+    with Session(engine) as session:
+            user_message = ConversationLogs(studentId=body.studentId, conversationId=UUID(body.conversationId), log=user_msg)  
+            session.add(user_message)
+            session.commit()
+            if len(assistant_resp_for_tool_call) != 0 and assistant_resp_for_tool_call is not None: 
+              print("ASSISTANT IN THE OTHER FIRST ONE")
+              print(assistant_resp_for_tool_call)
+              history = build_chat_history(assistant_resp_for_tool_call, body.query)
+              updated_messages.append(user_msg)
+              updated_messages.append({"role": "assistant", "content": assistant_resp_for_tool_call})
+              is_solved = steps_agent(updated_messages, steps)
+              assistant_msg = wrap_for_ql('assistant', assistant_resp_for_tool_call, is_solved)
+              print(assistant_msg)
+              bot_message = ConversationLogs(studentId=body.studentId, conversationId=UUID(body.conversationId), log=assistant_msg)
+              session.add(bot_message)
+              session.commit()
 
 # idea would be GET to get the conversation id and then route and post. 
 @router.post("/")
-
 async def wolfram_maths_response(body: StudentConversation): 
     print("COMPLETE BODY")
     print(body)
@@ -126,37 +167,20 @@ async def wolfram_maths_response(body: StudentConversation):
         if len(steps) != 0:
           updated_prompt = math_prompt(body.topic, body.level, updated_messages, body.query, steps, body.name)
           stream = open_ai(updated_prompt, updated_messages)
-          try: 
-            for chunk in stream:
+         
+          for chunk in stream:
               current_content = chunk.choices[0].delta.content
               if current_content is not None:
                 # print("outeer chunk")
                 # print(chunk.choices[0].delta.content, end="", flush=True)
                 assistant_resp_for_tool_call += chunk.choices[0].delta.content
                 yield current_content
-                await asyncio.sleep(0)
-          except asyncio.CancelledError:
-             print("caught cancelled error")
-             raise GeneratorExit
+                
+          
         # below save all to db 
         user_msg = wrap_for_ql('user', body.query)
+        BackgroundTasks.add_tasks(write_to_db_with_steps, body, user_msg, updated_messages, steps, assistant_resp_for_tool_call)
         print(user_msg)
-        with Session(engine) as session:
-            user_message = ConversationLogs(studentId=body.studentId, conversationId=UUID(body.conversationId), log=user_msg)  
-            session.add(user_message)
-            session.commit()
-            if len(assistant_resp_for_tool_call) != 0 and assistant_resp_for_tool_call is not None: 
-              print("ASSISTANT IN THE OTHER FIRST ONE")
-              print(assistant_resp_for_tool_call)
-              history = build_chat_history(assistant_resp_for_tool_call, body.query)
-              updated_messages.append(user_msg)
-              updated_messages.append({"role": "assistant", "content": assistant_resp_for_tool_call})
-              is_solved = steps_agent(updated_messages, steps)
-              assistant_msg = wrap_for_ql('assistant', assistant_resp_for_tool_call, is_solved)
-              print(assistant_msg)
-              bot_message = ConversationLogs(studentId=body.studentId, conversationId=UUID(body.conversationId), log=assistant_msg)
-              session.add(bot_message)
-              session.commit()
         yield "done with stream"
         return
       
@@ -206,49 +230,23 @@ async def wolfram_maths_response(body: StudentConversation):
       if len(steps) != 0:
         updated_prompt = math_prompt(body.topic, body.level, messages, body.query, steps, body.name)
         stream = open_ai(updated_prompt, messages)
-        try:
-          for chunk in stream:
+      
+        for chunk in stream:
             current_content = chunk.choices[0].delta.content
             if current_content is not None:
               #print("outeer chunk")
               #print(chunk.choices[0].delta.content, end="", flush=True)
               assistant_resp_for_tc += chunk.choices[0].delta.content
               yield current_content
-              await asyncio.sleep(0)
-        except asyncio.CancelledError:
-          print("caught cancelled error")
-          raise GeneratorExit
+              
+      
       # below save all to db 
       tc = messages[-1]
       user_msg = wrap_for_ql('user', body.query)
       log = json.dumps(user_msg)
       print(user_msg)
-      with Session(engine) as session:
-        user_message = ConversationLogs(studentId=body.studentId, conversationId=UUID(body.conversationId), log=user_msg)  
-        session.add(user_message)
-        session.commit()
-        if tc.get("role") == "function":
-          # save tc 
-          print("tool call",tc)
-          user_message = ConversationLogs(studentId=body.studentId, conversationId=UUID(body.conversationId), log=tc)  
-          session.add(user_message)
-          session.commit()
-        if len(assistant_resp) != 0 and assistant_resp is not None: 
-          assistant_msg = wrap_for_ql('assistant', assistant_resp)
-          bot_message = ConversationLogs(studentId=body.studentId, conversationId=UUID(body.conversationId), log=assistant_msg)
-          session.add(bot_message)
-          session.commit()
-        
-        if len(assistant_resp_for_tc) != 0 and assistant_resp_for_tc is not None: 
-          print("basically outside steps")
-          print(assistant_resp_for_tc)
-          history = build_chat_history(assistant_resp_for_tc, body.query)
-          is_solved = steps_agent(history, steps)
-          assistant_msg = wrap_for_ql('assistant', assistant_resp_for_tc, is_solved)
-          bot_message = ConversationLogs(studentId=body.studentId, conversationId=UUID(body.conversationId), log=assistant_msg)
-          session.add(bot_message)
-          session.commit()
-          print(assistant_msg)
+      BackgroundTasks.add_task(write_to_db, body, user_msg,steps,tc,assistant_resp, assistant_resp_for_tc)
+      
       yield "done with stream"
     chat_limit_check = os.environ.get("CHAT_LIMIT_CHECK")
     if(chat_limit_check != "disabled" and get_aitutor_chat_balance(body.firebaseId)):
